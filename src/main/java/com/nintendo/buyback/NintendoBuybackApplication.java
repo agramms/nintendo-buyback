86116b225c5f468a08ce95ccbb1203299b52610b
Alessandro Vieira Grammelsbacher <alessandro.grammelsbacher@gmail.com>
2017-03-02 18:05:43 -0300

    Spring Init - Definido pelo site http://start.spring.io/

---
+++ src/main/java/com/nintendo/buyback/NintendoBuybackApplication.java
@@ -0,0 +1,12 @@
+package com.nintendo.buyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NintendoBuybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NintendoBuybackApplication.class, args);
+	}
+}
